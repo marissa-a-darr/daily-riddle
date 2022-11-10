b09b65d2262d89ac130782750769f0daa2130e50
ejc10d <112822833+ejc10d@users.noreply.github.com>
2022-11-09 23:09:06 -0500

    fixed main branch from when changes were reverted

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,7 @@ var currentDate = moment().format('YYYY-MM-DD')
 
 var apiKey = "zD9N5mFUi2w04iTPmyfGTA==pEywnq16I5S1oOLc";
 
-var riddleQuestion = document.getElementById("riddle-question");
+var riddleQuestion = document.getElementById("riddleBox");
 //  var userAnswer = document.getElementById('')
 
 fetch("https://api.api-ninjas.com/v1/riddles", {
