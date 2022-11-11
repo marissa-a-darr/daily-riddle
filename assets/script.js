c38374e5fb3f521f90b173426e737e30dc87e9fc
ejc10d <112822833+ejc10d@users.noreply.github.com>
2022-11-10 20:21:40 -0500

    added logic for the submit button

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,25 @@
+
+// Code to get a new background image every day from NASA API
 var spaceBackground = $('body');
 
 var currentDate = moment().format('YYYY-MM-DD')
 // console.log(currentDate);
 
-  fetch('https://api.nasa.gov/planetary/apod?api_key=MMwg7IaYRbJGBkex5zb11KD8duZsipA3IO0JU1Gm&')
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.url);
-      apiBackground = data.url;
-      // console.log(apiBackground);
-      document.body.style.background = "url(" + apiBackground + ") no-repeat center";
-      
-  
-    });
+fetch('https://api.nasa.gov/planetary/apod?api_key=MMwg7IaYRbJGBkex5zb11KD8duZsipA3IO0JU1Gm&')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data.url);
+    apiBackground = data.url;
+    // console.log(apiBackground);
+    document.body.style.background = "url(" + apiBackground + ") no-repeat center";
 
+
+  });
+
+
+// Code to fetch riddle from a riddle API we found
 var apiKey = "zD9N5mFUi2w04iTPmyfGTA==pEywnq16I5S1oOLc";
 
 var riddleQuestion = document.getElementById("riddleBox");
@@ -39,3 +43,23 @@ fetch("https://api.api-ninjas.com/v1/riddles", {
       riddleQuestion.textContent = data[i].question;
     }
   });
+
+
+  // Submit button handler code
+  var submitBtn = $("#submitBtn");
+    
+    function getUserAnswer() {
+    var userInputVal = document.querySelector("#userAnswer").value;
+
+    if(!userInputVal) {
+      console.error("please enter an answer");
+      return;
+    }
+
+    console.log(userInputVal);
+  }
+
+submitBtn.click(function(event){
+  event.preventDefault();
+  getUserAnswer();
+});
