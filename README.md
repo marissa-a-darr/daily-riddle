3c6ddd5143fe75e1ee2875e5148a39441134925c
Marissa Darr <mrsaandee@gmail.com>
2022-11-16 23:37:34 -0500

    added notes for vanilla css, js, and html, updated readme with screenshot of webpage

--- README.md
+++ README.md
@@ -1,25 +1,31 @@
-# daily-riddle
-Title: Tricky Riddle
-CSS Framework: Skeleton
-API: 1: (NASA) https://api.nasa.gov/planetary/apod?api_key=MMwg7IaYRbJGBkex5zb11KD8duZsipA3IO0JU1Gm&date=2022-11-06
-2: (Riddles) https://api-ninjas.com/api/riddles
+# Out of This World Jokes
+Need a break from software development and challenging coding problems? Out of This World Jokes is designed to put a smile on your face with a joke. You may search using a keyword, category, or both! Upon entering/selecting your search parameters and submitting them, you will be display with a joke that fits your criteria. If you chose to not using any parameters, a random joke will be generated for you! Not only do we strive to lighten your mood with a laugh, but also provide you with the beauty of space. We use the beautiful photos NASA generates on a daily basis to implement a wonderful background for you. 
 
-AS A user
-I WANT to play a daily riddle game with a daily photo of NASA's space criteria
+## Installation
+N/A
+If you chose to continue work on this page, you will need to use the NASA's APOD API and the V2 JokeAPI from jokeapi.dev. 
 
-GIVEN the user wants to play a daily riddle game
-WHEN I visit the webpage
-THEN I am presented with daily riddle with a text input box to guess with a background photo of the NASA space photo
-WHEN I click into the user input box to enter my answer attempt
-THEN I am able to use the submit button for my attempt
-WHEN I guess incorrect
-THEN the page notifies me how many more attempts I have (with a total of 3 attempts)
-WHEN I answer correctly, the webpage notifies me that I am correct
+## Links 
+- [Repo](https://github.com/marissa-a-darr/outofthisworldjokes)
 
-Division of Work:
-Grabbing NASA photos and setting as background [Chang]
-Grabbing riddle with answer [Nicole]
-Building out HTML [Eliot/Nicole]
-Skeleton CSS [Eliot/Chang]
-JS: - creating modal/functionality of button [Marissa]
-functionality of localstorage/attempts [Marissa]
+- [URL](https://marissa-a-darr.github.io/outofthisworldjokes)
+
+## Screenshot 
+
+![Workday Scheduling App](/assets/css/screenshot.png)
+
+## Built With
+-HTML
+-CSS
+-JavaScript
+-Moment.Js
+-Jquery
+-Skeleton CSS
+-NASA APOD API
+-JokeAPI.dev V2 API 
+
+## Collaborated  By
+Marissa Darr [GitHub](https://github.com/marissa-a-darr)
+Eliot Crandall [GitHub](https://github.com/ejc10d)
+Chang Metellus [GitHub](https://github.com/LeeDaFool)
+Nicole Fallis [GitHub](https://github.com/nfallis96)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var spaceBackground = $("html");
-
+//grabs current date to be used in daily photo
 var currentDate = moment().format("YYYY-MM-DD");
-
+//daily nasa photo api fetch and setting it as background image
 fetch(
   "https://api.nasa.gov/planetary/apod?api_key=MMwg7IaYRbJGBkex5zb11KD8duZsipA3IO0JU1Gm&"
 )
@@ -13,42 +13,47 @@ fetch(
     document.body.style.backgroundImage =
       "url(" + apiBackground + ") ";
   });
-
-function getJoke(keyword) {
+//gets joke from api
+function getJoke() {
+  //core of api link to be built off of
   var getJokeAPI =
     "https://v2.jokeapi.dev/joke/"
 
-
+//user searches with keyword and format. edit api link to fit parameter guide line
   if(document.getElementById("searchInput").value && document.getElementById("format-input").value != "default") {
     getJokeAPI += document.getElementById("format-input").value + "?format=JSON&type=twopart&safe-mode&contains=" + (document.getElementById("searchInput").value);
+  //user searches for keyword only. edits api link to fit guide line
   } else if (document.getElementById("searchInput").value) {
     getJokeAPI += "Any?format=JSON&type=twopart&safe-mode&contains=" + (document.getElementById("searchInput").value);
+  //user searches for joke by format type only. edits api link to fit guide line
   } else if (document.getElementById("format-input").value != "default") {
     getJokeAPI += document.getElementById("format-input").value + "?format=JSON&type=twopart&safe-mode";
+  //user searches with no parameters at all. edits api link to fit
   } else {
     getJokeAPI += "Any?format=JSON&type=twopart&safe-mode";
   }
   console.log(getJokeAPI);
-
+//once the link is built based off of user parameters returns it to us json format to be used
   fetch(getJokeAPI)
     .then(function (response) {
       return response.json();
     })
+    //displays error message if no joke it found
     .then(function (data) {
       if (data.error) {
         $(".errorMess").text(data.message);
-        
+        //applies the api's joke set up and delivery to our html elements
       } else {
         $(".jokeSetup").text(data.setup);
         $(".jokePunch").text(data.delivery);
         console.log(data);}}
     )};
-
+//user clicks button, prevents from page reloading
 var btn = $("#submitBtn");
 btn.click(function (e) {
   e.preventDefault();
-  var userKeyword = $(".userSearch").val().toString();
-  getJoke(userKeyword);
+  //calls getJoke function, clears out old joke/punchline/error message upon searching to display only new search
+  getJoke();
   $(".jokeSetup").text("");
   $(".jokePunch").text("");
   $(".errorMess").text("");
